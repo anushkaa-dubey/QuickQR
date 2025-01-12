c86571d45da10867b0d0115ee374be8141042830
Anushka Dubey <136051483+anushkaa-dubey@users.noreply.github.com>
2025-01-12 17:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,44 +5,36 @@ const downloadBtn = document.getElementById('downloadBtn');
 const qrContainer = document.querySelector('.qr-body');
 
 let size = sizes.value;
-generateBtn.addEventListener('click',(e)=>{
+
+generateBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    isEmptyInput();
+    qrContainer.classList.add('fade-animation');
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");
 });
 
-sizes.addEventListener('change',(e)=>{
+sizes.addEventListener('change', (e) => {
     size = e.target.value;
-    isEmptyInput();
+    qrText.value.length > 0 && generateQRCode();
 });
 
-downloadBtn.addEventListener('click', ()=>{
-    let img = document.querySelector('.qr-body img');
+downloadBtn.addEventListener('click', () => {
+    const img = document.querySelector('.qr-body img');
 
-    if(img !== null){
-        let imgAtrr = img.getAttribute('src');
-        downloadBtn.setAttribute("href", imgAtrr);
-    }
-    else{
-        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`);
+    if (img) {
+        const imgAttr = img.getAttribute('src');
+        downloadBtn.setAttribute('href', imgAttr);
+    } else {
+        downloadBtn.setAttribute('href', `${document.querySelector('canvas').toDataURL()}`);
     }
 });
 
-function isEmptyInput(){
-    // if(qrText.value.length > 0){
-    //     generateQRCode();
-    // }
-    // else{
-    //     alert("Enter the text or URL to generate your QR code");
-    // }
-    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");;
-}
-function generateQRCode(){
+function generateQRCode() {
     qrContainer.innerHTML = "";
     new QRCode(qrContainer, {
-        text:qrText.value,
-        height:size,
-        width:size,
-        colorLight:"#fff",
-        colorDark:"#000",
+        text: qrText.value,
+        height: size,
+        width: size,
+        colorLight: "#fff",
+        colorDark: "#000",
     });
 }
